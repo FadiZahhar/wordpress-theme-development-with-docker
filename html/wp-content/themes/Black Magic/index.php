99ff11a9afd7c8a9aed10bb82bf2487781f807c4
Rana Jbeily <jbeilyrana@gmail.com>
2023-11-18 13:26:10 +0200

    content-loop changed

--- html/wp-content/themes/Black Magic/index.php
+++ html/wp-content/themes/Black Magic/index.php
@@ -1,4 +1,4 @@
 <?php get_header(); ?> <!-- calling header -->
-<?php get_template_part('templates/content','loop'); ?> <!-- calling template part -->
+<?php get_template_part('templates/content-loop','loop'); ?> <!-- calling template part -->
 <?php get_sidebar(); ?> <!-- calling sidebar -->
 <?php get_footer(); ?> <!-- calling footer -->
